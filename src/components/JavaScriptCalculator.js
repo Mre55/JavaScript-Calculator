d3d399b70248c5e0e7354c8afa5dcf46bf450fb3
mre55 <misganawmihreteab>
2021-09-16 18:33:59 +0300

    JavaScript calculator first commit

---
+++ src/components/JavaScriptCalculator.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './JavaScriptCalculator.css'
+
+class JavaScriptCalculator extends React.Component {
+
+    // constructor(props){
+    //       super(props);
+    // }
+    
+    render() {  
+
+      return (
+        <div>
+          <h1>This is good.</h1>
+        </div>
+      )
+    }
+}
+
+export default JavaScriptCalculator;
